b5d70e3864b64b75ebeac993bcb1a402abdad5c7
cloudguruab <ap.brown011@gmail.com>
2021-12-13 06:31:43 -0500

    final commit

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
 from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor #due to normal cursor not returning column names
-import psycopg2
-import os
+from sqlalchemy.orm import Session
+import models
 
-load_dotenv()
-
-try: 
-    conn=psycopg2.connect(
-        host='localhost',
-        database='fastapi',
-        user='postgres',
-        password=os.environ.get('PASS'),
-        cursor_factory=RealDictCursor
-    ) 
-    cursor = conn.cursor()
-    print("db connection resolved")
-except Exception as e:
-    print("db connection failed", e)
-    
 app = FastAPI()
+models.Base.metadata.create_all(bind=models.engine)
+
 
 class Post(BaseModel):
     """Defines schema of what client should send to the server"""
@@ -31,22 +18,50 @@ class Post(BaseModel):
     content: str
     published: bool = True
 
-
-@app.get('/') 
-def root():
-    return {"message": "Hello World"}
-
 @app.get("/posts")
-def get_posts():
-    return {"data": my_posts}
+def get_posts(db: Session = Depends(models.get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 @app.post("/create_posts")
-def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+def create_posts(post: Post, db: Session = Depends(models.get_db)):
+    new_post = models.Post(
+        **post.dict()
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
 
 @app.get(f"/posts/{id}")
-def get_post(id):
-    print(f'{id}')
+def get_post(id: int, db: Session = Depends(models.get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} not found')
+    
+    return {'post detail': post}
+
+@app.delete(f"/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(models.get_db)):
+    
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} not found')
+    post.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put(f'posts/{id}')
+def update_post(id: int, updated_post: Post, db: Session = Depends(models.get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
+    if post.first() == None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'post {id} not found')
+    post.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    
+    return {'data': post.first()}
---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (Column, Integer, String,
+                        Boolean, TIMESTAMP, create_engine,
+                        text)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get('URL') #TODO: docs
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        print('DB connection success')
+        yield db
+    finally:
+        db.close()
+        
+class Post(Base):
+    
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    create_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
